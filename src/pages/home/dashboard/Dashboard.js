bf2c0dd6fb31d54c46b34280f8f52be8a5d2c1d0
Akbar Afriansyah <abam.akbar07@gmail.com>
2021-03-29 16:45:53 +0700

    hide sidebar

--- src/pages/home/dashboard/Dashboard.js
+++ src/pages/home/dashboard/Dashboard.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Sidebar from '../components/Sidebar'
 
 const Dashboard = () => {
+   const [sideBar, setSideBar] = useState(true);
+   
+   const clickSideBar = () => {
+     setSideBar(!sideBar);
+   };
+
    return (
-      <div className="dashboard">
-         <Sidebar />
-      </div>
-   )
+     <div className="dashboard d-block">
+
+       <Sidebar sideBar={sideBar} clickSideBar={clickSideBar} />
+
+       <div className={sideBar ? "col-md-9 offset-md-3 mainContent" : "col-md-12 mainContent"}>
+         <h1 onClick={clickSideBar}>aw</h1>
+       </div>
+     </div>
+   );
 }
 
 export default Dashboard
